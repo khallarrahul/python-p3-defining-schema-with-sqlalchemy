6c55e3c2817bf4cbfed15e7bcce3195bf2940134
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-08-01 16:51:51 -0700

    alright

--- lib/debug.py
+++ lib/debug.py
@@ -4,7 +4,9 @@ from sqlalchemy import create_engine
 
 from sqlalchemy_sandbox import Student
 
-engine = create_engine('sqlite:///students.db')
+engine = create_engine("sqlite:///students.db")
 
-if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+if __name__ == "__main__":
+    import ipdb
+
+    ipdb.set_trace()
